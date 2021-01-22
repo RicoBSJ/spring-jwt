b261b2ff94c5cb4f68b0b09e0ab0c5db26c9aff7
RicoBSJ <aubrun.eric@free.fr>
2021-01-22 17:03:51 +0100

    FirstCommit

---
+++ src/main/java/com/aubrun/eric/spring/jwt/springjwt/sid/SpringJwtApplication.java
@@ -0,0 +1,13 @@
+package com.aubrun.eric.spring.jwt.springjwt.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtApplication.class, args);
+    }
+
+}
